4b9aff1e6e62ff1c2afc77fc57086fbf263bc3f3
Mihir1234567 <79329939+Mihir1234567@users.noreply.github.com>
2025-02-13 13:03:37 +0530

    form validation

--- learning25/src/components/forms/FormsDemo1.jsx
+++ learning25/src/components/forms/FormsDemo1.jsx
@@ -2,9 +2,39 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import "../forms/Forms.css";
 export const FormsDemo1 = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const submitHandler = (data) => {
     console.log(data);
+  };
+  const validator = {
+    nameValidator: {
+      required: {
+        value: true,
+        message: "*Please Enter Your Name",
+      },
+      minLength: {
+        value: 3,
+        message: "*Minimum length shoud be 3",
+      },
+    },
+    ageValidator: {
+      required: {
+        value: true,
+        message: "*Please Enter Your Age",
+      },
+      min: {
+        value: 18,
+        message: "*Minimum age 18",
+      },
+      max: {
+        value: 60,
+        message: "*Maximum age is 60",
+      },
+    },
   };
   return (
     <div class="form">
@@ -15,16 +45,18 @@ export const FormsDemo1 = () => {
           <input
             type="text"
             placeholder="Enter Your Full Name"
-            {...register("name")}
+            {...register("name", validator.nameValidator)}
           />
+          <span class="errormsg">{errors.name?.message}</span>
         </div>
         <div>
           <label htmlFor="age">Name:- </label>
           <input
             type="number"
             placeholder="Enter Your Age"
-            {...register("age")}
+            {...register("age", validator.ageValidator)}
           />
+          <span class="errormsg">{errors.age?.message}</span>
         </div>
         <div>
           <input type="submit" />
